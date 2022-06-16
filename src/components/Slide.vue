38a05f95ddf7bb9799a5a6117549fc7c0346f6d1
Kevin Fechner <kevin@mokka-merch.com>
2022-06-16 16:53:57 +0200

    chore: progress

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,17 +1,72 @@
 <template>
-    <div v-html="slide" class="cc-h-full"></div>
-    {{ slide }}
+    <div v-html="slide" :width="width" class="cc-h-full"></div>
 </template>
 <script setup lang="ts">
+import { onMounted, PropType, ref, watch } from 'vue';
+
 
 const props = defineProps({
     slide: {
-        type: HTMLElement,
+        type: String as PropType<string>,
         required: true,
     },
     width: {
         type: Number,
         required: true
+    },
+    sliderWidth: {
+        type: Number,
+        required: true
+    },
+    rootMounted: {
+        type: Boolean,
+        required: true
+    },
+    toShow: {
+        type: Number,
+        required: true
     }
 })
+
+const width = ref(0)
+
+watch(() => props.rootMounted,(val) => {
+    if (val) {
+        width.value = getWidthForSlide(props.slide)
+    }
+})
+
+/**
+ * 
+ * @param slide HTMLImageElement
+ */
+function getWidthForSlide(slide: string) :number {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(slide, 'text/html')
+    const img = doc.querySelector('img')
+    if (!img) return 0;
+
+    const imageWidth = img.width
+
+    return imageWidth
+
+    // console.log("🚀 ~ file: Slide.vue ~ line 50 ~ getWidthForSlide ~ imageWidth", imageWidth)
+    // const imageHeight = img.height
+    // const ratio = imageWidth / imageHeight
+
+    // // console.log("🚀 ~ file: Slide.vue ~ line 53 ~ getWidthForSlide ~ ratio", ratio)
+    // return (imageWidth) / props.sliderWidth 
+
+    // return ratio
+}
+
+defineExpose({
+    width,
+})
+
+onMounted(() => {
+  width.value = getWidthForSlide(props.slide)  
+})
+
+
 </script>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="carousel cc-flex cc-w-full" ref="root">
+    <div class="carousel cc-flex cc-w-f" ref="root">
         
         <slot name="navigation">
 
         </slot>
         <slot>
             <Slide v-for="(slide, index) in slides" :key="index" 
-                :slide="slide" class="" :width="getWidthForSlide(slide)">
+                :slide="slide" :sliderWidth="sliderWidth" class="" :width="getWidthForSlide(slide)" :rootMounted="rootMounted" :toShow="props.toShow">
             </Slide>
         </slot>
         <slot name="pagination">
@@ -19,9 +19,13 @@ import { onMounted, PropType, ref } from 'vue';
 import Slide from './Slide.vue'
 
 const root = ref<HTMLElement | null>(null)
+const rootMounted = ref(false)
+const sliderWidth = ref(0)
+const combinedWidth = ref(0)
+
 const props = defineProps({
     slides: {
-        type: Array as PropType<HTMLElement[]>,
+        type: Array as PropType<string[]>,
         required: true,
     },
     toShow: {
@@ -32,22 +36,41 @@ const props = defineProps({
 
 // function updateSlideWidth() {
 //     if (!root.value) return
-
 //     const rect = root.value.getBoundingClientRect()
-
 // }
 
-/**
- * 
- * @param slide HTMLImageElement
- */
-function getWidthForSlide(slide: HTMLElement) :number {
-    console.log(slide, props.toShow)
+function getWidthOfImage(slide: string) :number {
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(slide, 'text/html')
+    const img = doc.querySelector('img')
+    if (!img) return 0;
 
-    return 0
+    const imageWidth = img.width
+
+    return imageWidth
 }
 
+function getWidthForSlide(slide: string) :number {
+    // if ()
+    const imageWidth = getWidthOfImage(slide)
+
+    return imageWidth 
+}
+
+
+
 onMounted(() => {
+    if (root.value) {
+        const rect = root.value.getBoundingClientRect()
+        sliderWidth.value = rect.width
+        rootMounted.value = true
+
+        const slidesToShowWidth = props.slides.slice(0, props.toShow)
+        combinedWidth.value = slidesToShowWidth.reduce((acc, slide) => {
+            return acc + getWidthOfImage(slide)
+        }, 0)
+
+    }
 
 })
 </script>
