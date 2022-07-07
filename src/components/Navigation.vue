e71807f9b2c2472bbc71f28ccfbf757fc8d4b5fc
Kevin Fechner <kevin@mokka-merch.com>
2022-07-08 01:30:34 +0200

    fix(): navigation arrow position

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="sw-z-40">
         <slot name="leftArrow">
-            <LeftArrowIcon @click="$emit('prev')" class="sw-absolute sw-prev sw-top-0 sw-left-0 hover:sw-cursor-pointer" />
+            <LeftArrowIcon @click="$emit('prev')" class="sw-absolute sw-prev sw-top-[43%] sw-left-0 hover:sw-cursor-pointer" />
         </slot>
         <slot name="rightArrow">
-            <RightArrowIcon @click="$emit('next')" class="sw-absolute sw-next sw-top-0 sw-right-0 hover:sw-cursor-pointer"></RightArrowIcon>
+            <RightArrowIcon @click="$emit('next')" class="sw-absolute sw-next sw-top-[43%] sw-right-0 hover:sw-cursor-pointer"></RightArrowIcon>
         </slot>
     </div>
 </template>
