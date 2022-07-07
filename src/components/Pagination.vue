44b189af873567648b4b88688e3022d234943b28
Kevin Fechner <kevin@mokka-merch.com>
2022-07-08 01:45:07 +0200

    fix(lazyness): adding custom values statically

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -4,13 +4,20 @@
           <div class="wrapper sw-relative hover:sw-cursor-pointer" 
           :class="{'sw-bg-gold sw-bg-opacity-50 sw-z-10': index === currentIndex}" 
           @click="$emit('updateSlide', index)" 
-          v-html="photo">
+          >
+            <div v-html="photo" class="v-html-photo"></div>
+            <div class="sw-absolute sw-w-full sw-h-full sw-top-0" :class="{'sw-bg-gold sw-bg-opacity-50 sw-z-10': index === currentIndex}"></div>
+
           </div>
       </template>
     </div>
 </template>
 <script setup lang="ts">
 import { PropType } from 'vue';
+
+// const computedStyle = computed(() => {
+
+// })
 
 const props = defineProps({
     images: {
@@ -21,5 +28,14 @@ const props = defineProps({
         type: Number as PropType<number>,
         default: 0,
     },
+    activeStyle: {
+        type: Object as PropType<object>,
+        default: () => ({}),
+    },
 }) 
 </script>
+<style>
+.v-html-photo, .v-html-photo img {
+    height: 100%;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+				gold: '#C7AC7F',
+				blueish: '#006F7C',
+				black: '#141414',
+				'sb-grey': '#3A3A3B',
+				'white-dark': '#f2f2f2',
+				disabled: '#8c8c8c',
+			},
+    },
   },
   plugins: [],
   prefix: "sw-"
