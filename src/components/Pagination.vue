52c8a40c2a7df5efff1d7590d015531fa3232987
Kevin Fechner <kevin@mokka-merch.com>
2022-07-08 02:25:08 +0200

    fix(): sth

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="sw-flex sw-flex-row sw-justify-center gallery-preview-slide sw-gap-2 sw-mb-10" v-if="images">
+    <div class="sw-flex sw-flex-row sw-justify-center gallery-preview-slide sw-gap-2 sw-mb-10 sw-max-h-48" v-if="images">
       <template v-for="(photo, index) in images" :key="index">
           <div class="wrapper sw-relative hover:sw-cursor-pointer" 
           :class="{'sw-bg-gold sw-bg-opacity-50 sw-z-10': index === currentIndex}" 
