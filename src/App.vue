2d403cc6bfdc1cfcc62f6dce11a5a5466d613733
Kevin Fechner <kevin@mokka-merch.com>
2022-07-19 19:58:55 +0200

    fix(+max-height): update

--- src/App.vue
+++ src/App.vue
@@ -84,11 +84,21 @@ const bP = ref<BreakPoint>({
 
 
 <template>
-<div>
-  <Slider :slides="slides" :toShow="3" :breakPoints="bP">
+<div class="sw-flex sw-flex-col lg:sw-flex-row ">
+  <div class="lg:sw-w-2/3">
+    <Slider :slides="slides" :toShow="3" :breakPoints="bP" :showPagination="false">
+  
+    </Slider>
+  </div>
+  <div class="lg:sw-w-1/3 sw-bg-blueish sw-p-10 sw-text-sm sw-h-[500px]">
 
-  </Slider>
+  </div>
 </div>
+<!-- <div>
+      <Slider :slides="slides" :toShow="3" :breakPoints="bP" :showPagination="true">
+  
+    </Slider>
+</div> -->
 </template>
 
 <style>
